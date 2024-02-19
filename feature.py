b8c054d63e472616663ffc1be605b0165654c889
belleswetha <belleswetha@gmail.com>
2024-02-19 23:45:43 +0530

    first commit

---
+++ feature.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,render_template
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+
+FAI=Flask(__name__)
+
+@FAI.route('/Htmlforms',methods=['GET','POST'])
+
+def Htmlforms():
+    if request.method=='POST':
+      FD=request.form
+      un=FD["un"]
+      return un
+    return render_template('Htmlforms.html')
+
+
+class NameForm(Form):
+    name=StringField()
+    submit=SubmitField()
+
+
+@FAI.route('/Webforms',methods=['GET','POST'])
+def Webforms():
+    NFO=NameForm()
+    if request.method=='POST':
+        NFDO=NameForm(request.form)
+        if NFDO.validate():
+            return NFDO.name.data
+    return render_template('Webforms.html',NFO=NFO)
+
+
+if __name__=="__main__":
+    FAI.run(debug=True)
+
+
